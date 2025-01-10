746126845d88a68aeaca4d85aa7d8f06e6798c1e
PizzaKing29 <Legozap223@gmail.com>
2025-01-10 16:59:13 -0600

    changes

--- main/Program.cs
+++ main/Program.cs
@@ -3,7 +3,8 @@
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            Console.Clear();
             Console.WriteLine("Welcome to the game traveler, your journey awaits you.");
             Console.WriteLine("Your goal will be to walk along pathways and find gold,");
             Console.WriteLine("but be careful as some creatures in the forest can kill you while collecting the gold\n");
@@ -11,6 +12,7 @@
             bool playing = false;
             string[] items = {""};
             int gold = 0;
+            int health = 100;
 
             string? startInput = Console.ReadLine();
             startInput = startInput.ToUpper();
@@ -31,6 +33,7 @@
 
             while (playing)
             {
+                Console.WriteLine($"Your health is {health} points.");
                 Console.WriteLine($"You have {gold} gold.");
                 Console.WriteLine("Would you like to keep on walking? (Y/N)");
                 string? playingInput = Console.ReadLine();
@@ -39,29 +42,56 @@
 
                 if (playingInput == "Y")
                 {
+                    Console.Clear();
                     Console.WriteLine("Which pathway do you want to take?");
                     Console.WriteLine("forest or cave");
                     string? pathwayAnswer = Console.ReadLine();
                     pathwayAnswer = pathwayAnswer.ToLower();
+                    Console.Clear();
 
-                    Random random = new Random();
-                    int creatureChance = random.Next(1, 3);
+                    Random randomCreature = new Random();
+                    Random randomGold = new Random();
+                    Random randomHealth = new Random();
+                    int creatureChance = randomCreature.Next(1, 3);
+                    int goldFound = randomGold.Next(1, 26);
 
                     switch (pathwayAnswer)
                     {
                         case "forest":
                             if (creatureChance == 1)
                             {
+                                creatureChance = randomCreature.Next(1, 3);
+                                goldFound = randomGold.Next(1, 26);
+                                gold += goldFound;
+                                Console.WriteLine($"\nYou have found {goldFound} gold, awesome!");
                                 
                             }
                             else
                             {
-
+                                creatureChance = randomCreature.Next(1, 3);
+                                goldFound = randomGold.Next(1, 26);
+                                gold -= goldFound;
+                                Console.WriteLine($"\nYou have lost {goldFound} gold, and a bear has attacked you!");
+                                
                             }
                             break;
 
                         case "cave":
-
+                            if(creatureChance == 1)
+                            {
+                                creatureChance = randomCreature.Next(1, 3);
+                                goldFound = randomGold.Next(1, 26);
+                                gold += goldFound;
+                                Console.WriteLine($"\nYou have found {goldFound} gold, awesome!");
+                            }
+                            else
+                            {
+                                creatureChance = randomCreature.Next(1, 3);
+                                goldFound = randomGold.Next(1, 26);
+                                gold -= goldFound;
+                                Console.WriteLine($"\nYou have lost {goldFound} gold, and a snake has attacked you!");
+                                
+                            }
                             break;
                     }
 
@@ -77,7 +107,7 @@
                 }
 
                 }
-                Console.WriteLine("stats");
+                Console.WriteLine("Thanks for playing!");
             }
         }
     }
