d183738c66d3d15f8109064898ca3e4d917b0970
PizzaKing29 <Legozap223@gmail.com>
2025-01-12 21:56:35 -0600

    more features hgold

--- main/Program.cs
+++ main/Program.cs
@@ -1,18 +1,28 @@
-﻿namespace Main
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Main
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static bool playing = false;
+        public static string[] items = {""};
+        public static int gold = 0;
+        public static int health = 100;
+        public static Random randomCreature = new Random();
+        public static Random randomGold = new Random();
+        public static Random randomHealth = new Random();
+        public static int creatureChance = randomCreature.Next(1, 3);
+        public static int goldFound = randomGold.Next(1, 26);
+        public static int healthLost = randomHealth.Next(1, 33);
+
+        public static void Main(string[] args)
         {   
             Console.Clear();
             Console.WriteLine("Welcome to the game traveler, your journey awaits you.");
             Console.WriteLine("Your goal will be to walk along pathways and find gold,");
             Console.WriteLine("but be careful as some creatures in the forest can kill you while collecting the gold\n");
             Console.WriteLine("Start? (Y/N)");
-            bool playing = false;
-            string[] items = {""};
-            int gold = 0;
-            int health = 100;
 
             string? startInput = Console.ReadLine();
             startInput = startInput.ToUpper();
@@ -32,7 +42,7 @@
             }
 
 
-            while (playing && health > 1)
+            while (playing && health > 0)
             {
                 Console.WriteLine($"Your health is {health} points.");
                 Console.WriteLine($"You have {gold} gold.");
@@ -50,14 +60,33 @@
                     pathwayAnswer = pathwayAnswer.ToLower();
                     Console.Clear();
 
-                    Random randomCreature = new Random();
-                    Random randomGold = new Random();
-                    Random randomHealth = new Random();
-                    int creatureChance = randomCreature.Next(1, 3);
-                    int goldFound = randomGold.Next(1, 26);
-                    int healthLost = randomHealth.Next(1, 33);
+                    HandleEncounter(pathwayAnswer);
 
-                    switch (pathwayAnswer)
+                    
+                }
+                else if (playingInput == "N")
+                {
+                    playing = false;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid answer. \n");
+                }
+
+                }
+                GameOver();
+            }
+            public static void DisplayStats ()
+            {
+                Console.WriteLine($"Your health is {health} points.");
+                Console.WriteLine($"You have {gold} gold.");
+                Console.WriteLine("Would you like to keep on walking? (Y/N)");
+            }
+
+            public static void HandleEncounter (string pathwawyAnswer)
+            {
+                switch (pathwawyAnswer)
                     {
                         case "forest":
                             if (creatureChance == 1)
@@ -73,7 +102,11 @@
                                 creatureChance = randomCreature.Next(1, 3);
                                 goldFound = randomGold.Next(1, 26);
                                 healthLost = randomHealth.Next(1, 33);
-                                gold -= goldFound;
+
+                                if (gold > 0 && goldFound < gold) // makes sure gold cannot go in the negatives
+                                {
+                                    gold -= goldFound;
+                                }
                                 health -= healthLost;
                                 Console.WriteLine($"\nYou have lost {goldFound} gold, and a bear has attacked you!");
                                 Console.WriteLine($"You have also lost {healthLost} health, uh oh!\n");
@@ -94,27 +127,31 @@
                                 creatureChance = randomCreature.Next(1, 3);
                                 goldFound = randomGold.Next(1, 26);
                                 healthLost = randomHealth.Next(1, 33);
-                                gold -= goldFound;
+
+                                if (gold > 0 && goldFound < gold) // makes sure gold cannot go in the negatives
+                                {
+                                    gold -= goldFound;
+                                }
                                 health -= healthLost;
                                 Console.WriteLine($"\nYou have lost {goldFound} gold, and a snake has attacked you!");
                                 Console.WriteLine($"You have also lost {healthLost} health, uh oh!\n");
                                 
                             }
                             break;
+
+                        default:
+                            Console.WriteLine("Invalid pathway choice. Please type 'forest' or 'cave'.");
+                        break;
                     }
+            }
 
-                    
-                }
-                else if (playingInput == "N")
-                {
-                    playing = false;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid answer. \n");
-                }
+            public static void ValidateInput ()
+            {
 
-                }
+            }
+
+            public static void GameOver ()
+            {
                 Console.Clear();
                 Console.WriteLine("Thanks for playing!");
                 Console.WriteLine($"You had {health} health.");
@@ -126,12 +163,17 @@
                     Console.WriteLine("Thanks for playing! Unfortunatley you have died, yikes...");
                     Console.WriteLine($"But you ended with {gold} gold!");
                 }
-                else
+                else if (health < 1 && gold < 0)
                 {
                     Console.Clear();
                     Console.WriteLine("Thanks for playing! Unfortunatley you have died, yikes...");
                     Console.WriteLine($"You also ended with {gold} gold, better luck next time!");
                 }
+            }
+
+            public static void HiddenTreasure ()
+            {
+
             }
         }
     }
--- main/obj/Debug/net9.0/main.AssemblyInfo.cs
+++ main/obj/Debug/net9.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746126845d88a68aeaca4d85aa7d8f06e6798c1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f74c988483d02e213cb3c1c4036ae77eaebd965")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
